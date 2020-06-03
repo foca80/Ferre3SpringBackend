fff6a5bcdc60cc32d7c0c335e85d8e94599d31e7
javacf <flores.orihuela@gmail.com>
2020-06-03 08:56:52 -0500

    version desactivada para ecosistema microservicios

--- ferre3/src/main/java/pe/upc/ferre/Ferre2Application.java
+++ ferre3/src/main/java/pe/upc/ferre/Ferre2Application.java
@@ -2,14 +2,14 @@ package pe.upc.ferre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class Ferre2Application {
 
 	public static void main(String[] args) {
@@ -18,12 +18,12 @@ public class Ferre2Application {
 
 }
 
-@Configuration
+//@Configuration
 class RestTemplateConfig {
 
     // Crea un bean para que restTemplate pueda llamar a los servicios REST
-    @Bean
-    @LoadBalanced        // Load balance entre servicios corriendo en diferentes puertos.
+    //@Bean
+    //@LoadBalanced        // Load balance entre servicios corriendo en diferentes puertos.
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
